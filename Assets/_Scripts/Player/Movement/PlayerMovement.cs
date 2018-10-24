1facf19b0fe5146e3937236e29b71b316be9bf6a
NabilCherigui <mrtopgamer@hotmail.com>
2018-10-24 22:32:27 +0200

    PlayerMovement
    Slight adjustments made to PlayerMovement.cs

--- Assets/_Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/_Scripts/Player/Movement/PlayerMovement.cs
@@ -6,11 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float _speed;
-    [SerializeField] private string up, down, left, right, forward, back;
     
     private Rigidbody _rigidbody;
-    //private Input _forward, _back, _left, _right, _up, _down;
-    
+
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -18,20 +16,17 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButton("Space"))
-        {
-            print("oef");
-        }
-        Move(_speed, up, down, left, right, forward, back);
+        Move(_speed, "Horizontal", "Vertical");
     }
-
-    private void Move(float speed, string inputUp, string inputDown, string inputLeft, string inputRight, string inputForward, string inputBack)
+    
+    private void Move(float speed, string horizontal, string vertical)
     {
         var direction = new Vector3();
-        direction.y = (Input.GetButton(inputUp)) ? 1 : (Input.GetButton(inputDown)) ? -1 : 0;
-        direction.x = (Input.GetButton(inputForward)) ? 1 : (Input.GetButton(inputBack)) ? -1 : 0;
-        direction.z = (Input.GetButton(inputLeft)) ? 1 : (Input.GetButton(inputRight)) ? -1 : 0;
-        
-        _rigidbody.AddRelativeForce(direction * speed);
+        direction.x = Input.GetAxis(horizontal);
+        direction.z = Input.GetAxis(vertical);
+
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.AddForce(direction * speed, ForceMode.VelocityChange);
     }
+
 }
