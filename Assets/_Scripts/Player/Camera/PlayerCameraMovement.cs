a29b9941ff6d912f3ec5bb118c2b2cc9827433ac
NabilCherigui <mrtopgamer@hotmail.com>
2018-10-26 12:03:36 +0200

    Player Movement and Camera.
    Added a script dedicated to the camera's movement. Added an option for
    gravity in the players movement.

---
+++ Assets/_Scripts/Player/Camera/PlayerCameraMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraMovement : MonoBehaviour
+{
+
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _sensitivity;
+    [SerializeField] private float _minX;
+    [SerializeField] private float _maxX;
+    private float _clampX;
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    private void Update()
+    {
+        MoveCamera();
+    }
+	
+    private void MoveCamera()
+    {
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+
+        var rotationX = mouseX * _sensitivity;
+        var rotationY = mouseY * _sensitivity;
+
+        _clampX -= rotationY;
+        
+        var cameraRotation = _camera.transform.rotation.eulerAngles;
+        var bodyRotation = transform.rotation.eulerAngles;
+        
+        cameraRotation.x -= rotationY;
+        cameraRotation.z = 0;
+        bodyRotation.y += rotationX;
+
+        if(_clampX > _maxX)
+        {
+            _clampX = _maxX;
+            cameraRotation.x = _maxX;
+        }
+        else if(_clampX < _minX)
+        {
+            _clampX = _minX;
+            cameraRotation.x = 360 + _minX;
+        }
+
+        _camera.transform.rotation = Quaternion.Euler(cameraRotation);
+        transform.rotation = Quaternion.Euler(bodyRotation);
+        
+    }
+}
--- Assets/_Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/_Scripts/Player/Movement/PlayerMovement.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    [SerializeField] private float _speed;
+    public enum GravityMode
+    {
+        On,
+        Off
+    }
+    
+    public GravityMode gravityMode = GravityMode.On;
+    
+    [SerializeField] private float _speed, _gravity;
+    
+    [SerializeField] private string _horizontalAxis =  "Horizontal", _verticalAxis = "Vertical";
     
     private Rigidbody _rigidbody;
 
@@ -16,7 +25,15 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        Move(_speed, "Horizontal", "Vertical");
+        if (gravityMode == GravityMode.On)
+        {
+            Move(_speed, _horizontalAxis, _verticalAxis, _gravity);
+        }
+        else
+        {
+            Move(_speed, _horizontalAxis, _verticalAxis);
+        }
+        
     }
     
     private void Move(float speed, string horizontal, string vertical)
@@ -27,6 +44,17 @@ public class PlayerMovement : MonoBehaviour
 
         _rigidbody.velocity = Vector3.zero;
         _rigidbody.AddForce(direction * speed, ForceMode.VelocityChange);
+    }
+    
+    private void Move(float speed, string horizontal, string vertical, float gravity)
+    {
+        var direction = new Vector3();
+        direction.x = Input.GetAxis(horizontal) * speed;
+        direction.y = gravity;
+        direction.z = Input.GetAxis(vertical) * speed;
+
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.AddForce(direction, ForceMode.VelocityChange);
     }
 
 }
